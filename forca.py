ea33a6b427e6bb070b49abaab341721f31cceda7
Yuri <yuricoelho33@gmail.com>
2021-05-03 08:59:46 -0300

    Implementando o jogo da forca.

--- forca.py
+++ forca.py
@@ -4,6 +4,21 @@ def jogar():
     print("Bem-vindo ao jogo da forca!")
     print("********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou  = False
+
+    while(not enforcou and not acertou):
+
+        chute = input("Escolha uma letra para tentar ganhar adivinhas a palavra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Econtrei a letra {} na posição {} ".format(letra, index))
+            index += 1
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
