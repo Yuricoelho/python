79e7f2ce2602fed87cff5532baf4722c1b398c83
Yuri <yuricoelho33@gmail.com>
2021-05-03 10:56:24 -0300

    Implementando condições de parada de acordo com a vitória ou derrota do jogador.

--- forca.py
+++ forca.py
@@ -4,24 +4,35 @@ def jogar():
     print("Bem-vindo ao jogo da forca!")
     print("********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou  = False
+    erros    = 0
 
     while(not enforcou and not acertou):
 
         chute = input("Escolha uma letra para tentar ganhar adivinhas a palavra?")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index += 1
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
         print(letras_acertadas)
 
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
